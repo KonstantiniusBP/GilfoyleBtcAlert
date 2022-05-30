4d2c6657e9e0dfe88ad22a8431f7cb86f0498541
Konstantin [LastName] <papuvito123@gmail.com>
2022-05-30 13:10:56 +0300

    Made a few changes, and also changed BS to JSON

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 import requests
 from bs4 import BeautifulSoup
 import time
+import json
 
-crypto_url = 'https://www.binance.com/ru/price/bitcoin'
+crypto_url = 'https://api.coincap.io/v2/assets/bitcoin'
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.67 Safari/537.36'}
 
 def crypto_tracker():
-    html_page = requests.get(crypto_url, headers=headers)
-    soup = BeautifulSoup(html_page.content, 'html.parser')
-    crypto_price = soup.findAll("div", {"class": "css-12ujz79"})
-    print(crypto_price[0].text)
+    html_responce = requests.get(crypto_url, headers=headers)
+    json_data = html_responce.json()
+    print(json_data['data']['priceUsd'])
     time.sleep(5)
     crypto_tracker()
 crypto_tracker()
